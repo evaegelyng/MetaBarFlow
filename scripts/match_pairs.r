b6942e00a6a17663024b0417f75211a5bf2b9434
Eva Sigsgaard eva.sigsgaard@bios.au.dk <evaes@fe1.genomedk.net>
2020-04-28 16:55:06 +0200

    removed incorrect file.info()

--- scripts/match_pairs.r
+++ scripts/match_pairs.r
@@ -13,8 +13,8 @@ start.time <- Sys.time()
 
 #Matching the "sense" reads.
 if ( file.info(inputFiles[3])$size == 0 || file.info(inputFiles[4])$size == 0  ) {
-  print(paste(file.info(inputFiles[3]), " has size ", file.info(inputFiles[3])$size))
-  print(paste(file.info(inputFiles[4]), " has size ", file.info(inputFiles[4])$size)) } else {
+  print(paste(inputFiles[3], " has size ", file.info(inputFiles[3])$size))
+  print(paste(inputFiles[4], " has size ", file.info(inputFiles[4])$size)) } else {
    print(paste("processing ", inputFiles[3], "and ", inputFiles[4]))
    fastqPairedFilter(c(inputFiles[3],inputFiles[4]), c(outputFiles[3],outputFiles[4]), minLen=50, maxN=0, maxEE=2, truncQ=2,
                      matchIDs=TRUE)
@@ -24,8 +24,8 @@ if ( file.info(inputFiles[3])$size == 0 || file.info(inputFiles[4])$size == 0  )
 
 #Matching the "antisense" reads.
 if ( file.info(inputFiles[1])$size == 0 || file.info(inputFiles[2])$size == 0  ) {
-  print(paste(file.info(inputFiles[1]), " has size ", file.info(inputFiles[1])$size))
-  print(paste(file.info(inputFiles[2]), " has size ", file.info(inputFiles[2])$size)) } else {
+  print(paste(inputFiles[1], " has size ", file.info(inputFiles[1])$size))
+  print(paste(inputFiles[2], " has size ", file.info(inputFiles[2])$size)) } else {
    print(paste("processing ", inputFiles[1], "and ", inputFiles[2]))
    fastqPairedFilter(c(inputFiles[1],inputFiles[2]), c(outputFiles[1],outputFiles[2]), minLen=50, maxN=0, maxEE=2, truncQ=2,
                      matchIDs=TRUE)
