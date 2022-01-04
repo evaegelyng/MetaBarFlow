20c1c04202f2bb5cdb3050bd3819798ebcadebbd
evaegelyng <36068949+evaegelyng@users.noreply.github.com>
2022-01-04 15:10:56 +0100

    Update create_batch.sh

--- scripts/create_batch.sh
+++ scripts/create_batch.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+#
 # Script for creating the batch files required by demultiplex.sh 
 #
 # Author: Eva Egelyng Sigsgaard
@@ -5,8 +7,6 @@
 # The script produces a file (batchfileDADA2.list) in each sequencing library folder containing the fastq file names, the primer sequences, and the minimum length required for a read 
 # (unaligned, i.e. forward or reverse read) after trimming of primers and tags. Replace the primer sequences and length specified in the script with those appropriate for your project. 
 # If your primers contain inosine bases ("I"), these need to be replaced with "N", as the software does not recognize "I". 
-#
-#!/bin/bash
 
 for FOLDER_NAME in ./M*
 do
