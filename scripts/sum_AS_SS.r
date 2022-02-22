e2da1cd1d765df1a3e287761df02f56df61af9fa
evaegelyng <36068949+evaegelyng@users.noreply.github.com>
2022-02-22 09:14:15 +0100

    Update sum_AS_SS.r
    Replaced the custom sumSequenceTables function with the standard mergeSequenceTables function, which now has a "sum" setting.

--- scripts/sum_AS_SS.r
+++ scripts/sum_AS_SS.r
@@ -1,40 +1,7 @@
 # Merging the resulting tables of the "sense" and the "antisense" analyses
-# Authors: This script is based on the script C_Processing_with_DADA2.Rmd by Tobias G. Frøslev (see Frøslev et al. 2017). The function sumSequenceTables is a modified version of mergeSequenceTables, and was provided by Benjamin Callahan (see https://github.com/benjjneb/dada2/issues/132)
+# Authors: This script is based on the script C_Processing_with_DADA2.Rmd by Tobias G. Frøslev (see Frøslev et al. 2017). The custom function sumSequenceTables has been replaced with the standard mergeSequenceTables function, which now has a "sum" setting. 
 
 args = commandArgs(trailingOnly=TRUE)
-
-#Define a function for combining two or more sequence tables:  
-sumSequenceTables <- function(table1, table2, ..., orderBy = "abundance") {
-  # Combine passed tables into a list
-  tables <- list(table1, table2)
-  tables <- c(tables, list(...))
-  # Validate tables
-  if(!(all(sapply(tables, dada2:::is.sequence.table)))) {
-    stop("At least two valid sequence tables, and no invalid objects, are expected.")
-  }
-  sample.names <- rownames(tables[[1]])
-  for(i in seq(2, length(tables))) {
-    sample.names <- c(sample.names, rownames(tables[[i]]))
-  }
-  seqs <- unique(c(sapply(tables, colnames), recursive=TRUE))
-  sams <- unique(sample.names)
-  # Make merged table
-  rval <- matrix(0L, nrow=length(sams), ncol=length(seqs))
-  rownames(rval) <- sams
-  colnames(rval) <- seqs
-  for(tab in tables) {
-    rval[rownames(tab), colnames(tab)] <- rval[rownames(tab), colnames(tab)] + tab
-  }
-  # Order columns
-  if(!is.null(orderBy)) {
-    if(orderBy == "abundance") {
-      rval <- rval[,order(colSums(rval), decreasing=TRUE),drop=FALSE]
-    } else if(orderBy == "nsamples") {
-      rval <- rval[,order(colSums(rval>0), decreasing=TRUE),drop=FALSE]
-    }
-  }
-  rval
-}
 
 ###Sum sense and antisense sequence tables
 stAS <- file.path(args[1],"seqtab_AS_RDS")
@@ -45,8 +12,8 @@ seqtab.nochim_AS <- readRDS(stnsAS)
 seqtab.nochim_SS <- readRDS(stnsSS)
 seqtab_AS <- readRDS(stAS)
 seqtab_SS <- readRDS(stSS)
-sumtable <- sumSequenceTables(seqtab_SS,seqtab_AS) 
-nochim_sumtable <- sumSequenceTables(seqtab.nochim_SS,seqtab.nochim_AS)
+sumtable <- mergeSequenceTables(seqtab_SS,seqtab_AS, repeats="sum")
+nochim_sumtable <- mergeSequenceTables(seqtab.nochim_SS,seqtab.nochim_AS, repeats="sum")
 st <- file.path(args[1],"seqtab_RDS")
 stns <- file.path(args[1],"seqtab.nochim_RDS")
 saveRDS(sumtable,st)
